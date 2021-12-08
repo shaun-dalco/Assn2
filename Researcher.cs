6df37bdee9e5e5c9fb6349dc5cefa51382716012
V33v3rs <88295674+V33v3rs@users.noreply.github.com>
2021-12-08 12:15:49 +1100

    Entity Classes
    - Need methods of classes
    - Need to be placed in GITHUB within correct hierarchy 
    - Need to be tested w/ main driver
    - Need to be reviewed by team
    - Need to be reviewed by Amanda (Tutor) 
    
    - Henry

---
+++ Researcher.cs
@@ -0,0 +1,84 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assn2
+{
+
+    public enum Campus
+    {
+
+       Hobart,
+       Launceston,
+       CradelCoast
+
+    }
+
+    public enum Level
+    {
+
+        A,
+        B,
+        C,
+        D,
+        E
+
+    }
+
+    class Researcher
+    {
+        public int id;
+        public string givenName;
+        public string familyName;
+        public string jobTitle;
+        public string unit;
+        public Campus campus;
+        public string email;
+        public string photo;
+        public DateTime utasStart;
+        public DateTime currentStart;
+        public int publicationNum;
+
+        //Needs: Tenure method
+
+    }
+
+    class Position
+    {
+
+        public int id;
+        public Level level;
+        public DateTime start;
+        public DateTime finish; 
+
+        //Needs a constructor 
+
+
+    }
+
+    class Staff
+    {
+        public Level level;
+        public string[] superviseeName;
+        public Position[] positions;
+
+        //Needs: Initialiser, threeYearAverage method & performance method 
+
+
+
+    }
+
+    class Student
+    {
+
+        public string degree;
+        public string supervisor; 
+
+        //Need initialiser
+
+
+
+    }
+}
